71c5291f796b732a9cb7cf0d8744ec6a3290aa92
Nubpz <nhastly24@gmail.com>
2024-07-13 12:37:04 +0545

    Initial Commit

---
+++ Gritodo/GritodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GritodoApp.swift
+//  Gritodo
+//
+//  Created by Nabin Poudel on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct GritodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
